56b59ceb50240f69d5b22ca44754f75048c3441c
Shijie Sun <tageniu@gmail.com>
2025-10-27 00:28:03 -0400

    create blank proj

---
+++ ChatGPT-iOS/ChatGPT_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_iOSApp.swift
+//  ChatGPT-iOS
+//
+//  Created by b a on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPT-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatGPT-iOS
+//
+//  Created by b a on 10/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
